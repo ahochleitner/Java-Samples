6c5208b91f4ff5693fea432ae0ad6da4f154221a
Antonia Hochleitner <antoniahochleitner@Antonias-MBP-2.private.net>
2018-10-08 22:56:00 +0200

    ein erster Stanein erster Stand

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("hello world");
+    System.out.println();
+    printMemoryInfo();
+  }
+
+  public static void printMemoryInfo() {
+      Runtime ri = Runtime.getRuntime();
+      
+      System.out.println("avaiable Processors: " + ri.availableProcessors());
+      System.out.println("freeMemory: " + format(ri.freeMemory()));
+      System.out.println("totalMemory: " + format(ri.totalMemory()));
+      System.out.println("maxMemory: " + format(ri.maxMemory()));
+  }
+
+  public static String format(long in) {
+      return ""+(in/1024/1024);
+  }
+}
