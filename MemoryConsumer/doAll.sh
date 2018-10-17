ac8f9dd75c24db72b4634bb062a9d099abdc6f35
Andreas Hochleitner <hochleitner@lkw-walter.com>
2018-10-17 08:04:44 +0200

    docker login angemerkt

--- MemoryConsumer/doAll.sh
+++ MemoryConsumer/doAll.sh
@@ -2,6 +2,10 @@
 
 javac MemoryConsumer.java 
 jar cf memory_consumer.jar MemoryConsumer.class 
+
+echo "... docker login gemacht?"
+echo "... gegebenenfalls docker login lkw-dev-registry.artifactory.prod.lkw-walter.com machen"
+
 docker build -t memory_consumer .
 ./doArtifactoryUpload.sh 
 kubectl delete -f memory-consumer.yaml
