da691afa2491b40caf419c92836587ce2819d665
Andreas Hochleitner <hochleitner@lkw-walter.com>
2018-10-21 00:07:54 +0200

    kl verbesserung

--- CpuBench.java
+++ CpuBench.java
@@ -1,14 +1,15 @@
 public class CpuBench {
+  static long ITERATIONS = 100000000;
   public static void main(String[] argv)
   throws Exception
   {
       long start = System.currentTimeMillis();
-      for (int ii = 0 ; ii < 100000000 ; ii++)
+      for (int ii = 0 ; ii < ITERATIONS ; ii++)
       {
         String s = "this XXX a test".replaceAll("XXX", " is ");
       }
       long elapsed = System.currentTimeMillis() - start;
       System.out.println("elapsed time = " + elapsed + "ms");
-      System.out.println((elapsed * 1000.0) / 1000000 + " microseconds per execution");
+      System.out.println((elapsed * 1000.0) / ITERATIONS + " microseconds per execution");
   }
 }
