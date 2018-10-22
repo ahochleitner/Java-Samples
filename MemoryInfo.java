6175172bfdd93a569805c54d8ceb427aeca046b8
Andreas Hochleitner <hochleitner@lkw-walter.com>
2018-10-21 23:33:17 -0700

    kl. erweiterung

--- MemoryInfo.java
+++ MemoryInfo.java
@@ -1,10 +1,15 @@
 public class MemoryInfo {    
-    public static void main(String args[]) {
-                System.out.println("Used Memory   :  " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) + " bytes");
-                System.out.println("Free Memory   : " + Runtime.getRuntime().freeMemory() + " bytes");
-                System.out.println("Total Memory  : " + Runtime.getRuntime().totalMemory() + " bytes");
-                System.out.println("Max Memory    : " + Runtime.getRuntime().maxMemory() + " bytes");            
-        }
+  public static void main(String args[]) {
+    System.out.println("Used Memory   :  " + format((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())) + " MB");
+    System.out.println("Free Memory   : " + format(Runtime.getRuntime().freeMemory()) + " MB");
+    System.out.println("Total Memory  : " + format(Runtime.getRuntime().totalMemory()) + " MB");
+    System.out.println("Max Memory    : " + format(Runtime.getRuntime().maxMemory()) + " MB");            
+  }
+
+  public static String format(long in) {
+      return ""+(in/1024/1024);
+  }
+
 }
 
 // run with    java -Xms1025k -Xmx1025k -XshowSettings:vm  MemoryInfo
