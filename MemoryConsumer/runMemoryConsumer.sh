d54736168514a81c874502993d88ff5f5082ba1f
Andreas Hochleitner <hochleitner@lkw-walter.com>
2018-10-21 23:35:42 -0700

    erweiterung

--- MemoryConsumer/runMemoryConsumer.sh
+++ MemoryConsumer/runMemoryConsumer.sh
@@ -4,5 +4,5 @@ cd /opt/local/jars
 java -version
 
 javac MemoryConsumer.java
-#java MemoryConsumer
-java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap MemoryConsumer
+java MemoryConsumer
+#java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap MemoryConsumer
